782d9aafb34753e6072c1655d76e544f0b57b919
Shreya388 <meenaxjha@gmail.com>
2023-12-10 22:22:40 +0530

    added suggestion buttons

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
   const [albums, setAlbums] = useState([]);
+  const [caption, setCaption] = useState([""])
 
   useEffect(() => {
     // API Access Token
@@ -27,11 +28,56 @@ function App() {
       .catch(error => console.error('Error obtaining access token:', error));
   }, []);
 
+
+
+  async function taylor() {
+    try {
+      if (!accessToken) return console.log("Currently, no access token");
+
+      const getResponse = async (url) => await (await fetch(url, { method: 'GET', headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${accessToken}` } })).json();
+
+      const taylorSearchData = await getResponse('https://api.spotify.com/v1/search?q=Taylor%20Swift&type=artist');
+      const taylorArtistID = taylorSearchData.artists.items[0]?.id;
+
+      if (!taylorArtistID) return console.log("Taylor Swift's artist ID not found");
+
+      console.log("Taylor Swift's artist ID is " + taylorArtistID);
+
+      const taylorAlbumsData = await getResponse(`https://api.spotify.com/v1/artists/${taylorArtistID}/albums?include_groups=album&market=US&limit=50`);
+      setAlbums(taylorAlbumsData.items || []);
+
+      console.log(taylorAlbumsData);
+    } catch (error) {
+      console.error('There is Error', error);
+    }
+  }
+
+  async function arijit() {
+    try {
+      if (!accessToken) return console.log("Currently, no access token");
+
+      const getResponse = async (url) => await (await fetch(url, { method: 'GET', headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${accessToken}` } })).json();
+
+      const arijitSinghData = await getResponse('https://api.spotify.com/v1/search?q=Arijit%20Singh&type=artist');
+      const arijitID = arijitSinghData.artists.items[0]?.id;
+
+      if (!arijitID) return console.log("Taylor Swift's artist ID not found");
+
+      console.log("Arijit artist ID is " + arijitID);
+
+      const arijitAlbumsData = await getResponse(`https://api.spotify.com/v1/artists/${arijitID}/albums?include_groups=album&market=US&limit=50`);
+      setAlbums(arijitAlbumsData.items || []);
+
+      console.log(arijitAlbumsData);
+    } catch (error) {
+      console.error('There is Error', error);
+    }
+  }
+
   // Search
   async function search() {
     try {
       console.log("Search for " + searchInput);
-
       if (!accessToken) {
         console.log("Access token is not available yet. Waiting for it...");
         return;
@@ -70,6 +116,7 @@ function App() {
     } catch (error) {
       console.error('Error during search:', error);
     }
+
   }
 
   return (
@@ -78,38 +125,41 @@ function App() {
         <Container>
           <Navbar.Brand className='text-white'>LOGO</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto mx-auto">
-            <Nav.Link href="#home">Trending</Nav.Link>
-            <Nav.Link href="#link">Famous</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto mx-auto">
+              <Nav.Link href="#home">Trending</Nav.Link>
+              <Nav.Link href="#link">Famous</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
       <div className="header text-white text-center">
-      <Container>
-        <div className='p-4'>
-        <h1>Listen to Your<br /> Favorite Albums</h1>
-        <p>Explore Millions of Songs</p>
-        </div>
+        <Container>
+          <div className='p-4'>
+            <h1>Listen to Your<br /> Favorite Albums</h1>
+            <p>Explore Millions of Songs</p>
+          </div>
 
-        <InputGroup className="" size="lg">
-          <FormControl
-            placeholder="Search for album"
-            type="input"
-            className='searchbox'
-            onKeyPress={event => {
-              if (event.key === "Enter") {
-                search();
-              }
-            }}
-            onChange={event => setSearchInput(event.target.value)}
-          />
-          <Button onClick={search} className='searchbtn'>
-            Search
-          </Button>
-        </InputGroup>
-      </Container>
+          <InputGroup className="" size="lg">
+            <FormControl
+              placeholder="Search for album"
+              type="input"
+              className='searchbox'
+              onKeyPress={event => {
+                if (event.key === "Enter") {
+                  search();
+                }
+              }}
+              onChange={event => setSearchInput(event.target.value)}
+            />
+            <Button onClick={search} className='searchbtn'>
+              Search
+            </Button>
+
+          </InputGroup>
+          <Button onClick={taylor} className='caption'>Taylor Swift</Button>
+          <Button onClick={arijit} className='caption'>Arijit Singh</Button>
+        </Container>
       </div>
       <Container>
         <Row className="mx-2 mt-4 row row-cols-3">
@@ -119,7 +169,6 @@ function App() {
               <Card.Body>
                 <Card.Title>{album.name}</Card.Title>
                 <p>{album.album_type}</p>
-
                 <a href={album.external_urls.spotify}>Listen</a>
               </Card.Body>
             </Card>
